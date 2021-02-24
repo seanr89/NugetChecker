2de211b701a9cb74a41db83a6074101083984c91
seanr89 <srafferty89@gmail.com>
2021-02-24 19:07:36 +0000

    New project and sln setup in the works!

---
+++ ConsoleNuget/NugConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
