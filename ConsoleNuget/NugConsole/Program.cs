751163700e9fadd7851f588da81cb7f66fcbf49c
seanr89 <srafferty89@gmail.com>
2021-02-24 21:45:15 +0000

    new appsettings and setup DI

--- ConsoleNuget/NugConsole/Program.cs
+++ ConsoleNuget/NugConsole/Program.cs
@@ -4,9 +4,46 @@ namespace NugConsole
 {
     class Program
     {
+        private static IConfigurationRoot Configuration { get; set; }
+        private static string env { get; set; }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!\r");
+            Console.WriteLine("\n");
+            Configuration = LoadAppSettings();
+
+            var serviceCollection = new ServiceCollection();
+            RegisterAndInjectServices(serviceCollection, Configuration);
         }
+
+        /// <summary>
+        /// Query app settings json content
+        /// </summary>
+        /// <returns></returns>
+        private static IConfigurationRoot LoadAppSettings()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+                return config;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("Error trying to load app settings");
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Prep/Configure Dependency Injection
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="config"></param>
+        private static void RegisterAndInjectServices(IServiceCollection services, IConfiguration config)
+        { }
     }
 }
